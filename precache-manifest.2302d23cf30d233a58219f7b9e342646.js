316ed8d99760cee9be568d7a553c8d4b076c816c
Preetham <preethamms619@gmail.com>
2020-06-30 00:26:43 +0530

    Updates

---
+++ precache-manifest.2302d23cf30d233a58219f7b9e342646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657688941945092ecebe4bf899ec0b60",
+    "url": "/PreethamMSalpha/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "468e529bfa3093e91515",
+    "url": "/PreethamMSalpha/monsters-rolodex/static/css/main.33d3a52e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/PreethamMSalpha/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PreethamMSalpha/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468e529bfa3093e91515",
+    "url": "/PreethamMSalpha/monsters-rolodex/static/js/main.c5e07275.chunk.js"
+  },
+  {
+    "revision": "349eb9ef023d45d9d506",
+    "url": "/PreethamMSalpha/monsters-rolodex/static/js/runtime-main.b1e03291.js"
+  }
+]);
